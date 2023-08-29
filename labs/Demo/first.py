286a1e62c1df0e01984cb40560bb343becb1eb98
Jemima Hsu <jemimahsu@yahoo.co.uk>
2023-08-29 14:39:08 +0100

    Initial commit + Ex 1.2 + demo

---
+++ labs/Demo/first.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+# Author: JHsu
+# Version: 1.0
+# Description: First program in IDLE!
+
+# name = "Jemima Hsu"
+# print("My name is", name) # Comma separates and adds a space
+# print("My name is " + name) # Put in extra space
+
+name = input("Enter your name: ")
+
+import math
+print("Cosine of 0.5=", math.cos(0.5))
+
+import random
+print("My lucky number is", random.randint(1,50))
---
+++ labs/Exercise 1/Starter/Ex1_2.py
@@ -0,0 +1,8 @@
+# Author: JH
+# Exercise 1.2 - Compile and run a program
+
+first = "Jemima"
+last = "Hsu"
+
+print(first, last)
+print(first + " " + last)

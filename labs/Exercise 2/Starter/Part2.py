c96173f8ef0950cffc4633c05771ed7da60d7de4
Jemima Hsu <jemimahsu@yahoo.co.uk>
2023-08-29 17:42:38 +0100

    Ex 2.2 + 3

---
+++ labs/Exercise 2/Starter/Part2.py
@@ -0,0 +1,11 @@
+title = "Thor:Ragnarok"
+director = "Taika Waititi"
+year = 2017
+duration = 130
+cert = "12A"
+
+print(title, "was directed by", director, "and released in", year, "with certificate",
+      cert, "running for", duration, "minutes.")
+
+title2 = input("Name a movie.")
+director2 = input("Who was the director?")

fa88b10ada7dbcd472893c01cf7cdf505f76158d
Jemima Hsu <jemimahsu@yahoo.co.uk>
2023-08-30 12:02:35 +0100

    Ex 3.3 + 3.4

--- labs/Exercise 3/Starter/MovieLoop.py
+++ labs/Exercise 3/Starter/MovieLoop.py
@@ -1,1 +1,8 @@
 movies = ["Jaws", "Star Wars", "Vanilla Sky", "The Mission", "Bridget Jones Diary 2"]
+
+for movie in enumerate(movies, 1):
+    length = len(movie[1])
+    # print(type(movie[0])) = int
+    # print(type(movie[1])) = str
+    # print(type(length)) = int
+    print(str(movie[0]) + ".", movie[1] + ",", str(length))
